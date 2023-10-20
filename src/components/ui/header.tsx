b798dc6677d58e6efe2d7d39ab301e227ed6994a
AlexandreDresch <z3r0pc@protonmail.com>
2023-10-20 14:50:29 -0300

    feat: header menu update

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,16 +1,45 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import { HomeIcon, ListOrderedIcon, LogInIcon, MenuIcon, PercentCircleIcon, PercentIcon, ShoppingCartIcon } from "lucide-react";
 import { Button } from "./button";
 import { Card, CardContent } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 export default function Header() {
   return (
-    <Card className="flex justify-between p-5 items-center">
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+    <Card className="flex items-center justify-between p-5">
+      <Sheet>
+        <SheetTrigger>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
 
-      <h1 className="font-semibold text-lg text-primary">
-        Drive <span className="text-white">Emporium</span>
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+
+          <div className="mt-2 flex flex-col gap-2">
+          <Button variant="outline" className="w-full justify-start gap-2">
+              <HomeIcon size={16}/> Home
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <LogInIcon size={16}/> Sign In
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <PercentIcon size={16} /> Offers
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <ListOrderedIcon size={16} /> Catalog
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+
+      <h1 className="text-xl font-semibold text-primary text-justify">
+        Drive <span className="text-white text-xs">EMPORIUM</span>
       </h1>
 
       <Button size="icon" variant="outline">
