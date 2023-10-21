ed3fbd0fd769d93c477cfc8e6f734046b95c8982
AlexandreDresch <z3r0pc@protonmail.com>
2023-10-21 15:20:21 -0300

    feat: home page first banner

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-01.png"
+        width={0}
+        height={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="Up to 25% off this month only."
+      />
+    </div>
+  );
+}
