9bdc7d24e375e5c204866334da9f412b1ec57117
AlexandreDresch <z3r0pc@protonmail.com>
2023-10-21 20:36:58 -0300

    feat: footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center bg-accent px-8 py-4">
+      <p className="text-xs opacity-75">
+        © 2023 Copyright <span className="font-semibold">Drive Emporium</span>
+      </p>
+    </footer>
+  );
+}
