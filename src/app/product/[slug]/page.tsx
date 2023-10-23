6480a3ca4a5c2d567e2ffcf9278b1743b71b33cf
AlexandreDresch <z3r0pc@protonmail.com>
2023-10-23 18:10:27 -0300

    feat: recommended products in product details page

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import ProductDetails from "@/components/ui/productDetails";
+import ProductHorizontalList from "@/components/ui/productHorizontalList";
 import ProductImages from "@/components/ui/productImages";
+import SectionTitle from "@/components/ui/sectionTitle";
 import { generateProductFinalPrice } from "@/helpers/product";
 import { prismaClient } from "@/lib/prisma";
 
@@ -14,14 +16,32 @@ export default async function Product({ params: { slug } }: ProductPageProps) {
     where: {
       slug,
     },
+    include: {
+      category: {
+        include: {
+          products: {
+            where: {
+              slug: {
+                not: slug,
+              },
+            },
+          },
+        },
+      },
+    },
   });
 
   if (!product) return null;
 
   return (
     <div className="flex flex-col gap-6">
-        <ProductImages imageUrls={product.imageUrls} name={product.name}/>
-        <ProductDetails product={generateProductFinalPrice(product)}/>
-  </div>
+      <ProductImages imageUrls={product.imageUrls} name={product.name} />
+      <ProductDetails product={generateProductFinalPrice(product)} />
+
+      <div className="mb-6">
+        <SectionTitle>More in {product.category.name}</SectionTitle>
+        <ProductHorizontalList products={product.category.products} />
+      </div>
+    </div>
   );
 }
