bc2e33338d28c0e451beb76b1fa6953cd9285945
AlexandreDresch <z3r0pc@protonmail.com>
2023-10-26 14:46:22 -0300

    feat: header buttons going to pages

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -89,9 +89,16 @@ export default function Header() {
               </Button>
             )}
 
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <HomeIcon size={16} /> Home
-            </Button>
+            <SheetClose asChild>
+              <Link href="/">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <HomeIcon size={16} /> Home
+                </Button>
+              </Link>
+            </SheetClose>
 
             <Button variant="outline" className="w-full justify-start gap-2">
               <PercentIcon size={16} /> Offers
