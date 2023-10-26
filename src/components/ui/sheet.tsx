ff3e396fa39edb75a4a80e64d8553962bdd915aa
AlexandreDresch <z3r0pc@protonmail.com>
2023-10-25 21:12:16 -0300

    fix: sheet deploy error

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -14,9 +14,11 @@ const SheetTrigger = SheetPrimitive.Trigger
 const SheetClose = SheetPrimitive.Close
 
 const SheetPortal = ({
+  // @ts-expect-error - `className` is not a valid prop for `DialogPortal`
   className,
   ...props
 }: SheetPrimitive.DialogPortalProps) => (
+  // @ts-expect-error - `className` is not a valid prop for `DialogPortal`
   <SheetPrimitive.Portal className={cn(className)} {...props} />
 )
 SheetPortal.displayName = SheetPrimitive.Portal.displayName
