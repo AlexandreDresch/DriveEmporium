ada69ebd41204a6a30f1da41d2a615a9280f26bc
AlexandreDresch <z3r0pc@protonmail.com>
2023-10-31 19:47:14 -0300

    feat: add variant to badge

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -15,6 +15,7 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
+        heading: "w-fit gap-1 border-[1px] border-primary px-3 py-1 text-center uppercase"
       },
     },
     defaultVariants: {
