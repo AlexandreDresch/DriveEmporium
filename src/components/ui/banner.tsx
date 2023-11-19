212abbd8bc8213665d51d307b3719b13da11eed1
AlexandreDresch <z3r0pc@protonmail.com>
2023-11-19 00:55:59 -0300

    feat: add banner component variations

--- src/components/ui/banner.tsx
+++ src/components/ui/banner.tsx
@@ -1,14 +1,103 @@
-import Image, { ImageProps } from "next/image";
+import Image from "next/image";
 
-export default function Banner({ alt, ...props }: ImageProps) {
-  return (
-    <Image
-      width={0}
-      height={0}
-      className="h-auto w-full px-5"
-      sizes="100vw"
-      {...props}
-      alt={alt}
-    />
+type BaseBannerProps = {
+  variant: "base";
+  percentage: number;
+  text: string;
+  image?: never;
+  textOrientation?: never;
+};
+
+type DetailedBannerProps = {
+  variant: "detailed";
+  percentage: number;
+  text: string;
+  image: string;
+  textOrientation: "left" | "right";
+};
+
+type DesktopBannerProps = {
+  variant: "desktop";
+  percentage: number;
+  text: string;
+  image?: never;
+  textOrientation?: never;
+};
+
+type BannerProps = BaseBannerProps | DetailedBannerProps | DesktopBannerProps;
+
+export default function Banner({
+  percentage,
+  variant,
+  image,
+  text,
+  textOrientation,
+}: BannerProps) {
+  return variant === "base" ? (
+    <div className="mx-5 flex items-center justify-center gap-9 rounded-md bg-gradient-to-r from-primary to-accent/95 p-5 sm:gap-14">
+      <div className="text-white">
+        <div className="flex justify-between">
+          <div className="text-base font-light">
+            <p>up</p>
+            <p>to</p>
+          </div>
+
+          <p className="text-5xl font-bold">
+            {percentage}
+            <span className="text-3xl">%</span>
+          </p>
+        </div>
+
+        <p className="w-full text-end font-bold">off</p>
+        <p className="text-base font-light">{text}</p>
+      </div>
+
+      <Image
+        width={0}
+        height={0}
+        className="h-32 w-32 object-contain sm:h-36 sm:w-36"
+        sizes="100vw"
+        priority
+        src="/basket-shopping.png"
+        alt="Basket Shopping"
+      />
+    </div>
+  ) : variant === "detailed" ? (
+    <div
+      className={`mx-5 flex items-center justify-center gap-9 rounded-md bg-gradient-to-r p-5 sm:gap-14 ${
+        textOrientation === "right"
+          ? "flex-row-reverse from-[#36393C] to-[#36393C]/40"
+          : "from-[#36393C]/40 to-[#36393C]"
+      }`}
+    >
+      <div className="text-white">
+        <div className="flex justify-between">
+          <div className="text-base font-light">
+            <p>up</p>
+            <p>to</p>
+          </div>
+
+          <p className="text-5xl font-bold text-primary">
+            {percentage}
+            <span className="text-3xl">%</span>
+          </p>
+        </div>
+
+        <p className="w-full text-end font-bold">off</p>
+        <p className="text-base font-light">on {text}</p>
+      </div>
+
+      <Image
+        width={0}
+        height={0}
+        className="h-auto w-32 object-contain sm:w-36"
+        sizes="100vw"
+        priority
+        src={image ? image : "/basket-shopping.png"}
+        alt={text}
+      />
+    </div>
+  ) : (
+    <div>desktop</div>
   );
 }
