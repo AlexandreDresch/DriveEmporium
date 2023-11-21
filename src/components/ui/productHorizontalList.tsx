bcdeff6b26880732aee84e055a007ccbde3181ce
AlexandreDresch <z3r0pc@protonmail.com>
2023-11-20 23:49:41 -0300

    feat: add scrollbar to desktop mode

--- src/components/ui/productHorizontalList.tsx
+++ src/components/ui/productHorizontalList.tsx
@@ -1,22 +1,21 @@
-import { Product } from "@prisma/client"
+import { Product } from "@prisma/client";
 import ProductItem from "./productItem";
 import { generateProductFinalPrice } from "@/helpers/product";
 
 interface ProductHorizontalListProps {
-    products: Product[];
+  products: Product[];
 }
 
-export default function ProductHorizontalList({ products }: ProductHorizontalListProps) {
+export default function ProductHorizontalList({
+  products,
+}: ProductHorizontalListProps) {
   return (
-    <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
-        {
-            products.map((product) => (
-              <div className="w-[170px] min-w-[170px]" key={product.id}>
-                <ProductItem product={generateProductFinalPrice(product)} />
-              </div>
-            ))
-        }
-
+    <div className="flex w-full gap-4 overflow-x-auto px-5 md:overflow-x-scroll md:[&::-webkit-scrollbar-thumb]:rounded-lg md:[&::-webkit-scrollbar-thumb]:bg-accent [&::-webkit-scrollbar]:invisible ">
+      {products.map((product) => (
+        <div className="w-[170px] min-w-[170px] md:mb-5" key={product.id}>
+          <ProductItem product={generateProductFinalPrice(product)} />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
