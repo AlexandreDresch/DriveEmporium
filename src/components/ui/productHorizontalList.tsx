261faad2bf77b67ef7d91c01197359dac0101062
AlexandreDresch <z3r0pc@protonmail.com>
2023-11-23 23:47:39 -0300

    feat: add desktop style to product list

--- src/components/ui/productHorizontalList.tsx
+++ src/components/ui/productHorizontalList.tsx
@@ -10,9 +10,9 @@ export default function ProductHorizontalList({
   products,
 }: ProductHorizontalListProps) {
   return (
-    <div className="flex w-full gap-4 overflow-x-auto px-5 md:overflow-x-scroll md:[&::-webkit-scrollbar-thumb]:rounded-lg md:[&::-webkit-scrollbar-thumb]:bg-accent [&::-webkit-scrollbar]:invisible ">
+    <div className="flex w-full gap-4 overflow-x-auto md:overflow-x-scroll md:[&::-webkit-scrollbar-thumb]:rounded-lg md:[&::-webkit-scrollbar-thumb]:bg-accent [&::-webkit-scrollbar]:invisible ">
       {products.map((product) => (
-        <div className="w-[170px] min-w-[170px] md:mb-5" key={product.id}>
+        <div className="w-[170px] lg:w-[264px] min-w-[170px] lg:min-w-[264px] md:mb-5" key={product.id}>
           <ProductItem product={generateProductFinalPrice(product)} />
         </div>
       ))}
