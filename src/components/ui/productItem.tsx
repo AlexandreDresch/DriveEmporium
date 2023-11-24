a95eb0da7c5fbd7e3ca369d8da4b4e02866d0dcd
AlexandreDresch <z3r0pc@protonmail.com>
2023-11-23 23:47:54 -0300

    feat: add desktop style to product item

--- src/components/ui/productItem.tsx
+++ src/components/ui/productItem.tsx
@@ -11,7 +11,7 @@ export default function ProductItem({ product }: ProductItemProps) {
   return (
     <Link href={`/product/${product.slug}`}>
       <div className="flex flex-col gap-2">
-        <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
+        <div className="relative flex h-[170px] lg:h-[300px] w-full items-center justify-center rounded-lg bg-accent">
           <Image
             src={product.imageUrls[0]}
             height={0}
@@ -32,7 +32,7 @@ export default function ProductItem({ product }: ProductItemProps) {
         </div>
 
         <div className="flex flex-col gap-1">
-          <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+          <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm lg:text-base">
             {product.name}
           </p>
           <div className="flex items-center gap-2">
