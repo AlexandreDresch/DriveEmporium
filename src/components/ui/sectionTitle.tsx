093b3eb86cba0627139909bbc884bbfe9c57a73b
AlexandreDresch <z3r0pc@protonmail.com>
2023-11-23 23:49:59 -0300

    fix: remove padding from Section Title

--- src/components/ui/sectionTitle.tsx
+++ src/components/ui/sectionTitle.tsx
@@ -5,7 +5,7 @@ export default function SectionTitle({
   ...props
 }: ComponentProps<"p">) {
   return (
-    <p className="mb-2 pl-5 font-semibold uppercase" {...props}>
+    <p className="mb-2 font-semibold uppercase" {...props}>
       {children}
     </p>
   );
