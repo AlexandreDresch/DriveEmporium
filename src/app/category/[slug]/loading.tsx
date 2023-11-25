04a76e07590692aff0a907b36aa5ffb5278980e4
AlexandreDresch <z3r0pc@protonmail.com>
2023-11-24 23:22:15 -0300

    feat: update skeletons

--- src/app/category/[slug]/loading.tsx
+++ src/app/category/[slug]/loading.tsx
@@ -3,17 +3,18 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function CategoryLoading() {
   return (
-    <div className="flex flex-col gap-8 p-5 lg:container lg:mx-auto">
+    <div className="flex flex-col gap-8 p-5 lg:px-24">
       <Badge variant="outline" className="border-0">
-        <Skeleton className="flex h-6 w-28 rounded-full bg-accent" />
+        <Skeleton className="h-6 w-28 rounded-full bg-accent" />
       </Badge>
 
-      <div className="grid grid-cols-2 gap-8 md:grid-cols-4 lg:grid-cols-6 lg:gap-10">
-        {[...Array(6)].map((x, i) => (
-          <Skeleton
-            className="flex h-[200px] w-full rounded-tl-lg rounded-tr-lg bg-accent"
-            key={i}
-          />
+      <div className="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
+        {[...Array(6)].map((_, i) => (
+          <div key={i} className="flex flex-col gap-2">
+            <Skeleton className="flex h-[170px] w-full items-center justify-center rounded-lg bg-accent lg:h-[300px]" />
+            <Skeleton className="h-4 w-1/2 rounded-md bg-accent" />
+            <Skeleton className="h-4 w-1/3 rounded-md bg-accent" />
+          </div>
         ))}
       </div>
     </div>
